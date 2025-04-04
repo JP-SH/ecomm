a851674d1074ec0e43ef55f835572d155126c158
Jasdeep Singh <j.singhhh917@gmail.com>
2025-04-04 20:03:39 +1100

    finsl touches to compare passwords

--- repositories/users.js
+++ repositories/users.js
@@ -51,9 +51,9 @@ class UsersRepository {
 
     // this line is the exact same as the 3 lines above
     const [hashed, salt] = saved.split('.')
-    const hashedSupplied = await scrypt(supplied, salt, 64);
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
 
-    return hashed === hashedSupplied;
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   async writeAll(records) {
