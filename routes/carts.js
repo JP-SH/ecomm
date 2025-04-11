65a1b37a39213bf10601cd4d97ffacf7216f2ff2
Jasdeep Singh <j.singhhh917@gmail.com>
2025-04-11 13:40:34 +1000

    finishing shooping carts boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Recieve a POST request to add an item to a cart
+
+// Recieve a GET request to show all items in cart
+
+// Recieve a POST request to delete an item from a cart
+
+module.exports = router;
