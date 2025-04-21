3fad29bf62b272d10b5d62fce0427ea4758936c4
Jasdeep Singh <j.singhhh917@gmail.com>
2025-04-21 18:29:29 +1000

    moving validations into new file

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -5,6 +5,10 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
+
+
+
 
 const router = express.Router();
 
@@ -13,38 +17,18 @@ router.get('/signup', (req, res) => {
 });
 
 router.post('/signup', [
-  check('email')
-    .trim()
-    .normalizeEmail()
-    .isEmail()
-    .withMessage('Must be a valid email')
-    .custom( async (email) => {
-      const existingUser = await usersRepo.getOneBy({ email });
-      if (existingUser) {
-        throw new Error('Email in use');
-      }
-    }),
-  check('password')
-    .trim()
-    .isLength({ min: 4, max: 20 })
-    .withMessage('Must be between 4 and 20 characters'),
-  check('passwordConfirmation')
-    .trim()
-    .isLength({ min: 4, max: 20 })
-    .withMessage('Must be between 4 and 20 characters')
-    .custom( async (passwordConfirmation, { req }) => {
-      if (passwordConfirmation !== req.body.password) {
-        throw new Error('Passwords must match');
-      }
-    })
-],  async (req, res) => {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation
+],
+  async (req, res) => {
   const errors = validationResult(req);
   const { email, password, passwordConfirmation } = req.body;
 
   // Create a user in our user repo to represent this person
   const user = await usersRepo.create({ email, password });
 
-  // Store the id of that user inside the users cookie
+  // Store the id of that user inside the us   ers cookie
   req.session.userId = user.id;
 
   res.send('Account created!!!');
